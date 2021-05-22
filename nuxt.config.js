b556284a09f23d92a997d4a90c453aa030785e50
Rohan Lamb <lambrohan@gmail.com>
2021-05-22 12:37:54 +0530

    styles-not-loading-fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,27 +7,29 @@ export default {
   head: {
     title: 'Netaji Safe Company',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Netaji Safe Company | Manufacturer of best in class lockers, fire proof safe, strong room door, depository safe, lockers for home, school lockers in Kolhapur' }
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Netaji Safe Company | Manufacturer of best in class lockers, fire proof safe, strong room door, depository safe, lockers for home, school lockers in Kolhapur',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       {
-        rel: "preconnect",
-        href:
-          'https://fonts.googleapis.com/css2?family=Spartan:wght@300;400;500;600;700&display=swap',
-        crossorigin: 'crossorigin',
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Spartan:wght@300;400;500;600;700&display=swap',
       },
-    ]
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -42,19 +44,14 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxtjs/device',
-  ],
+  buildModules: ['@nuxtjs/device'],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/style-resources',
-  ],
+  modules: ['@nuxtjs/style-resources'],
   styleResources: {
     scss: ['~assets/global.scss'],
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
