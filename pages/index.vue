703466aefdf77275e1a018473166cfa7bb268590
nikhil18-9192 <nikhilkorane08@gmail.com>
2021-06-02 16:27:50 +0530

    latest

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home-page">
     <Hero />
-    <ProductCarousel v-if="!$device.isMobile" />
-    <PhoneProductCarousel v-else />
+    <ProductCarousel />
+    <!-- <PhoneProductCarousel v-else /> -->
     <ButtonComponent />
   </div>
 </template>
