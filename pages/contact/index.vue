1224e6c31633e93883451addb99c8c59ab4bc910
nikhil18-9192 <nikhilkorane08@gmail.com>
2021-06-16 11:32:20 +0530

    loading spinner added

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -65,7 +65,17 @@
           <input v-model="subject" type="text" placeholder="Subject" />
           <input v-model="number" type="number" placeholder="Phone Number" />
           <textarea v-model="message" placeholder="Message"></textarea>
-          <button @click="submit">send</button>
+          <button @click="submit">
+            <div v-if="loading" class="sk-chase">
+              <div class="sk-chase-dot"></div>
+              <div class="sk-chase-dot"></div>
+              <div class="sk-chase-dot"></div>
+              <div class="sk-chase-dot"></div>
+              <div class="sk-chase-dot"></div>
+              <div class="sk-chase-dot"></div>
+            </div>
+            <span v-else>send</span>
+          </button>
         </div>
       </div>
     </div>
@@ -98,6 +108,7 @@ export default {
       subject: '',
       number: '',
       message: '',
+      loading: false,
     }
   },
   methods: {
@@ -115,12 +126,15 @@ export default {
         return
       }
       try {
+        this.loading = true
         await this.submitToServer()
         this.clearForm()
         this.$toast.success(
           'Thank you for contacting us, we will respond to you shortly!'
         )
+        this.loading = false
       } catch (error) {
+        this.loading = false
         this.$toast.error(
           'Sorry! Something went wrong. Please try again later.'
         )
@@ -338,6 +352,96 @@ export default {
           color: #fff;
           text-transform: capitalize;
           cursor: pointer;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          margin-left: auto;
+          margin-right: auto;
+          .sk-chase {
+            width: 28px;
+            height: 25px;
+            position: relative;
+            animation: sk-chase 2.5s infinite linear both;
+          }
+
+          .sk-chase-dot {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            left: 0;
+            top: 0;
+            animation: sk-chase-dot 2s infinite ease-in-out both;
+          }
+
+          .sk-chase-dot:before {
+            content: '';
+            display: block;
+            width: 25%;
+            height: 25%;
+            background-color: #fff;
+            border-radius: 100%;
+            animation: sk-chase-dot-before 2s infinite ease-in-out both;
+          }
+
+          .sk-chase-dot:nth-child(1) {
+            animation-delay: -1.1s;
+          }
+          .sk-chase-dot:nth-child(2) {
+            animation-delay: -1s;
+          }
+          .sk-chase-dot:nth-child(3) {
+            animation-delay: -0.9s;
+          }
+          .sk-chase-dot:nth-child(4) {
+            animation-delay: -0.8s;
+          }
+          .sk-chase-dot:nth-child(5) {
+            animation-delay: -0.7s;
+          }
+          .sk-chase-dot:nth-child(6) {
+            animation-delay: -0.6s;
+          }
+          .sk-chase-dot:nth-child(1):before {
+            animation-delay: -1.1s;
+          }
+          .sk-chase-dot:nth-child(2):before {
+            animation-delay: -1s;
+          }
+          .sk-chase-dot:nth-child(3):before {
+            animation-delay: -0.9s;
+          }
+          .sk-chase-dot:nth-child(4):before {
+            animation-delay: -0.8s;
+          }
+          .sk-chase-dot:nth-child(5):before {
+            animation-delay: -0.7s;
+          }
+          .sk-chase-dot:nth-child(6):before {
+            animation-delay: -0.6s;
+          }
+
+          @keyframes sk-chase {
+            100% {
+              transform: rotate(360deg);
+            }
+          }
+
+          @keyframes sk-chase-dot {
+            80%,
+            100% {
+              transform: rotate(360deg);
+            }
+          }
+
+          @keyframes sk-chase-dot-before {
+            50% {
+              transform: scale(0.4);
+            }
+            100%,
+            0% {
+              transform: scale(1);
+            }
+          }
         }
       }
     }
